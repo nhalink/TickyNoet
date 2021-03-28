b6355ecec010f11560c7f89564b4b3bf625f8c9b
Niko Halink <nhalink@gmail.com>
2021-03-28 20:57:08 +0200

    Temporary coming soon page

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="nl">
+	<head>
+		<meta charset="utf-8">
+		<title>Ticky Noet ... Nicky Toet ... Nick ... eten met Nick</title>
+		<meta name="description" content="De site van Ticky Noet, ook wel bekend als Nick van Eten met Nick op Youtube">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/normalize.min.css">
+		<link rel="stylesheet" href="css/style.min.css">
+		<style>
+			#msg {
+				font-family: Helvetica, Arial, sans-serif;
+				font-size: 150px;
+				font-weight: bold;
+				line-height: 1.1em;
+				color: #000;
+				text-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
+				width: 600px;
+				text-align: center;
+				margin: 300px auto;
+			}
+		</style>
+	</head>
+	<body>
+<!--
+Hee Nick,
+
+Als je dit leest, ik heb dit domein niet gekaapt om het voor veel poen aan je te verkopen
+Ik heb het gereserveerd zodat juist niemand anders dat doet
+Wil je het domein hebben dan kan je hem voor € 0,- (nul euro) krijgen
+Stuur een mailtje naar mineminemine@tickynoet.nl zodat ik het kan overdragen
+Wil je het domein niet, ook prima, dan blijft het zo staan
+//-->
+		<p id="msg">COMING SOON</p>
+
+		<script>
+			window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+			ga('create', 'UA-264537974-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
+		</script>
+		<script src="https://www.google-analytics.com/analytics.js" async></script>
+	</body>
+</html>
